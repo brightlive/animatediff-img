61ad6f6c1be4448942c2593382d72342251c3209
Brian <71035007+bright-brian@users.noreply.github.com>
2023-09-25 20:01:32 -0700

    Set alpha permanently at 0.1

--- animatediff/pipelines/pipeline_animation.py
+++ animatediff/pipelines/pipeline_animation.py
@@ -330,7 +330,8 @@ class AnimationPipeline(DiffusionPipeline):
                     for i in range(video_length):
                         # I just feel dividing by 30 yield stable result but I don't know why
                         # gradully reduce init alpha along video frames (loosen restriction)
-                        init_alpha = (video_length - float(i)) / video_length / 30 
+                        #init_alpha = (video_length - float(i)) / video_length / 30 
+                        init_alpha = 0.1
                         latents[:, :, i, :, :] = init_latents * init_alpha + latents[:, :, i, :, :] * (1 - init_alpha)
         else:
             if latents.shape != shape:
